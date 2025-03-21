0bf630a3c56bfbe98fad5a2581f58bbdc2ed2b6d
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-03-21 13:51:42 +0700

    add list

--- basic/dataframe_creation.R
+++ basic/dataframe_creation.R
@@ -11,3 +11,32 @@ summary(df)
 # Indexing and Selection
 # Select first column or days column
 df[,1]
+
+# Select value of vector in rain column
+df[,'rain']
+# equivalent of df$rain or df[['rain]] with double brackets [[]]
+
+# Select Dataframe with only rain column
+df['rain']
+
+# Select rows 1 to 3 with columns days and rain
+df[1:3, c('days', 'rain')]
+
+# Subseting
+# return only rows that rain (rain == TRUE)
+subset(df, subset = rain==T)
+
+# return only rows that temp over 23
+subset(df, subset = temp > 23)
+
+# Sorting
+sorted.temp <- order(df['temp'])
+sorted.temp
+# 5 1 3 2 4
+df[sorted.temp,]
+
+# descending
+desc.temp <- order(-df['temp'])
+desc.temp
+# 4 2 3 1 5
+df[desc.temp,]
---
+++ basic/list.R
@@ -0,0 +1,9 @@
+a <- c('a','b','c')
+num <- c(1,2,3)
+logical_v <- c(T,F,T)
+
+my.list = list(alphabet = a, number = num, bool = logical_v)
+my.list
+my.list$bool
+
+typeof(my.list$bool)
