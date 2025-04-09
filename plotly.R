a44b2f4e1e472f3f3883b44788a084d007a600ec
Jiranathanan Siripun <jiranathanan@gmail.com>
2025-04-09 23:56:53 +0700

    Plotly

---
+++ plotly.R
@@ -0,0 +1,11 @@
+#https://plot.ly
+#install.packages('plotly')
+library(ggplot2)
+library(plotly)
+
+pl <- ggplot(mtcars, aes(mpg,wt)) + geom_point()
+#print(pl)
+
+gpl <- ggplotly(pl)
+
+print(gpl)
